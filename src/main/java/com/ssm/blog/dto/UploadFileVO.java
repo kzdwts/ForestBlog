294eaf5c5bebced6186e5cf41b1044c1e7bbad3f
kangyong <kangyong_wts@sina.com>
2019-07-28 18:02:39 +0800

    整理文件

---
+++ src/main/java/com/ssm/blog/dto/UploadFileVO.java
@@ -0,0 +1,20 @@
+package com.ssm.blog.dto;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description: 文件上传返回结果
+ * @author: kangyong
+ * @date: 2019/7/28 18:00
+ * @version: v1.0
+ */
+@Data
+public class UploadFileVO {
+
+    private String src;
+
+    private String title;
+
+}
